cf3c218f3df74e277fa6c8624b774e3f32116aa0
Lauren Klein <leklein@calpoly.edu>
2017-05-12 19:45:13 -0700

    t

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar target/spring-boot-heroku-1.0.0-SNAPSHOT.jar
--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -5,23 +5,21 @@ import base.studentFlowchartDb.FlowchartRepository;
 import com.mongodb.MongoClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableMongoRepositories
 public class Application {
 
-    @Autowired private static CatalogCourseRepository catalogCourseRepository;
-    @Autowired private static FlowchartRepository studentFlowchartRepository;
+    //@Autowired private static CatalogCourseRepository catalogCourseRepository;
+    //@Autowired private static FlowchartRepository studentFlowchartRepository;
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class, args);
-        MongoClient mongoClient = new MongoClient();
-        //MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-
-        //MongoDatabase database = mongoClient.getDatabase("SlochartDatabase");
-        //MongoCollection catalogCourses = database.getCollection("CatalogCourses");
-
-        //mongoClient.close(); //TODO on application shutdown
     }
 }
