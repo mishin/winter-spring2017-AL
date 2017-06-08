1972057e136c6a0bbca7933d3734a972c1063743
vmtai <vivian46959@gmail.com>
2017-06-07 21:07:25 -0700

    Change ordering of classes and commented out dbseeder

--- src/main/java/base/catalogDb/DbSeeder.java
+++ src/main/java/base/catalogDb/DbSeeder.java
@@ -46,37 +46,37 @@ public class DbSeeder implements CommandLineRunner {
     public void run(String ... strings) throws Exception {
         //This code is run every time the project is deployed, locally or on heroku.
         //If you want to use this to seed, be aware that it will permanently affect the database.
-        catalogRepository.deleteAll();
-        List<CatalogCourseJson> jsonCourses = new ArrayList<CatalogCourseJson>();
-        List<CatalogCourse> courses = new ArrayList<CatalogCourse>();
-        Scanner in = new Scanner(new FileReader("coursecatalog.txt"));
-        Gson gson = new Gson();
-        String[] json = in.nextLine().split("},");
-
-        for (int i = 0; i < json.length; i++) {
-            json[i] += "}";
-            jsonCourses.add(gson.fromJson(json[i], CatalogCourseJson.class));
-            CatalogCourseJson temp = jsonCourses.get(i);
-            courses.add(new CatalogCourse(temp.getCourseId(),
-                    temp.getPrefix() + " " + temp.getNumber(),
-                    temp.getTitle(),
-                    temp.getDescription(),
-                    convertPrereq(temp.getPrerequisites()),
-                    4));
-        }
-
-        for (CatalogCourse c: courses) {
-            List<String> prereqId = new ArrayList<String>();
-            for(String p: c.getPrerequisites()) {
-                String id = containsName(courses, p);
-                if (id != null) prereqId.add(id);
-            }
-            c.setPrerequisites(prereqId);
-
-        }
-
-        in.close();
-        catalogRepository.insert(courses);
+//        catalogRepository.deleteAll();
+//        List<CatalogCourseJson> jsonCourses = new ArrayList<CatalogCourseJson>();
+//        List<CatalogCourse> courses = new ArrayList<CatalogCourse>();
+//        Scanner in = new Scanner(new FileReader("coursecatalog.txt"));
+//        Gson gson = new Gson();
+//        String[] json = in.nextLine().split("},");
+//
+//        for (int i = 0; i < json.length; i++) {
+//            json[i] += "}";
+//            jsonCourses.add(gson.fromJson(json[i], CatalogCourseJson.class));
+//            CatalogCourseJson temp = jsonCourses.get(i);
+//            courses.add(new CatalogCourse(temp.getCourseId(),
+//                    temp.getPrefix() + " " + temp.getNumber(),
+//                    temp.getTitle(),
+//                    temp.getDescription(),
+//                    convertPrereq(temp.getPrerequisites()),
+//                    4));
+//        }
+//
+//        for (CatalogCourse c: courses) {
+//            List<String> prereqId = new ArrayList<String>();
+//            for(String p: c.getPrerequisites()) {
+//                String id = containsName(courses, p);
+//                if (id != null) prereqId.add(id);
+//            }
+//            c.setPrerequisites(prereqId);
+//
+//        }
+//
+//        in.close();
+//        catalogRepository.insert(courses);
     }
 
 }
